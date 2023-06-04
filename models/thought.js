5b4f15dfa5e8b8a130f5cf4bfa0bafa168784096
marvin blunck <marvinblunck@gmail.com>
2023-06-03 17:41:22 -0700

    models are built

---
+++ models/thought.js
@@ -0,0 +1,35 @@
+const { Schema, Types } = require('mongoose');
+const reactionSchema = require('./reaction');
+ 
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length 
+  })
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/, "email is invalid"],
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: "thought",
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: "user"
+        }]
+    }, {
+    toJSON: {
+        getters: true,
+    },
+})
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length 
+  })
+const User = model('user', userSchema);
+module.exports = User;
